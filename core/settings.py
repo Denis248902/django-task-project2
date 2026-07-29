be85eb9927f4d800b4b39a4f289733c36e3d4eef
Денис Постовойтов <denisdotniy@yandex.ru>
2026-07-29 16:41:32 +0300

    feat: add media settings, connect media URLs, fix gallery template

--- core/settings.py
+++ core/settings.py
@@ -149,3 +149,12 @@ CACHES = {
         "LOCATION": "unique-snowflake",
     }
 }
+
+import os
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+import os
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
