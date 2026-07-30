85fee37f46a97f2920eddd7073fa7a87d16f87d6
Денис Постовойтов <denisdotniy@yandex.ru>
2026-07-30 19:00:22 +0300

    auth: JWT fully verified via curl and test script, K5 closed

--- core/urls.py
+++ core/urls.py
@@ -3,8 +3,11 @@ from django.conf.urls.static import static
 from django.contrib import admin
 from django.contrib.auth import views as auth_views
 from django.urls import include, path
+from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 
 urlpatterns = [
+    path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
     path("admin/", admin.site.urls),
     path(
         "login/",
